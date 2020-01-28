945059d88fe4da9077e5bd21983f1dc51d0e7823
Nata Pinheiro <nata.ps@hotmail.com>
2020-01-27 21:41:25 -0300

    Conectando firebase

--- js/documentos.js
+++ js/documentos.js
@@ -4,5 +4,13 @@ function search(){
     validation(cpf);
 }
 function validation(cpfvalue){
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class","ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de:' +cpfvalue;
+
+}
+function back(){
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class","ocultar");
+    document.getElementById('inputCPF').value = '';
 }
---
+++ js/initFirebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBen4ZQbZ6CwOsjbw95vfjy19wYqBdoKRg",
+    authDomain: "documentos-55008.firebaseapp.com",
+    databaseURL: "https://documentos-55008.firebaseio.com",
+    projectId: "documentos-55008",
+    storageBucket: "documentos-55008.appspot.com",
+    messagingSenderId: "1060866988738",
+    appId: "1:1060866988738:web:5ee91de2cf43e85703395b",
+    measurementId: "G-Z3KBKJYNHJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
