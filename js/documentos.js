8ad6e52486a5dcebc7921c3de47df55f3f276bfb
Nata Pinheiro <nata.ps@hotmail.com>
2020-01-28 20:48:23 -0300

    Busca da pasta por CPF

--- js/documentos.js
+++ js/documentos.js
@@ -4,13 +4,28 @@ function search(){
     validation(cpf);
 }
 function validation(cpfvalue){
+   
+
+    var storage = firebase.storage();
+    // Retorna uma promisses que será processada
+    storage.ref().child(cpfvalue).listAll().then(function(todosArquivos){
+        if(todosArquivos.items.length >= 1){
+            next(cpfvalue);
+        } else {
+            alert('CPF não Encontrado')
+        }
+    }).catch(function(error){
+        console.log('ERRO', error);
+    });
+}
+function next(cpfvalue){
     document.getElementById('busca').setAttribute("class", "ocultar");
     document.getElementById('resultado').removeAttribute("class","ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de:' +cpfvalue;
 
 }
 function back(){
-    document.getElementById('busca').setAttribute("class", "ocultar");
-    document.getElementById('resultado').removeAttribute("class","ocultar");
+    document.getElementById('busca').removeAttribute("class","ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
     document.getElementById('inputCPF').value = '';
 }
