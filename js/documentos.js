25ec1e970b50777236ecf837e9d3835d72d74ba9
Nata Pinheiro <nata.ps@hotmail.com>
2020-01-29 21:02:56 -0300

    Listagem dos itens com links

--- js/documentos.js
+++ js/documentos.js
@@ -3,19 +3,44 @@ function search(){
     console.log(cpf);
     validation(cpf);
 }
-function validation(cpfvalue){
-   
-
+    function validation(cpfvalue){
     var storage = firebase.storage();
     // Retorna uma promisses que será processada
     storage.ref().child(cpfvalue).listAll().then(function(todosArquivos){
         if(todosArquivos.items.length >= 1){
+            listFiles(cpfvalue);
             next(cpfvalue);
         } else {
             alert('CPF não Encontrado')
         }
     }).catch(function(error){
         console.log('ERRO', error);
+    });
+}
+function listFiles(cpfvalue){
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de:' +cpfvalue;
+    var storage = firebase.storage();
+    var arquivos;
+    var nomeArquivos = [];
+    var linksArquivos = [];
+    storage.ref().child(cpfvalue).listAll().then(function(todosArquivos){
+        arquivos = todosArquivos.items;
+        console.log(arquivos);
+        for(let i=0; i<arquivos.length; i++){
+            nomeArquivos.push(arquivos[i].name);
+            storage.ref(cpfvalue+'/'+nomeArquivos[i]).getDownloadURL().then(function(url) {
+                console.log(url);
+                var ul = document.getElementById("list");
+                var li = document.createElement("li");
+                var listItem = '<a href="'+url+'"target="_blank">'+nomeArquivos[i]+'</a>';
+                li.innerHTML = listItem;
+                ul.appendChild(li);
+                linksArquivos.push(url);
+            }).catch(function(error){
+                console.log(error);
+                          
+            });
+        }
     });
 }
 function next(cpfvalue){
